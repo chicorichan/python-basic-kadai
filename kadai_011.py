8a11e9771be0d79599a4f586bbb03846db71e1c2
chicorichan <157950805+chicorichan@users.noreply.github.com>
2024-03-08 06:12:24 +0900

    11章課題をアップロード

---
+++ kadai_011.py
@@ -0,0 +1,10 @@
+array = ["水","金","地","火","木","土","天","海","冥"]
+
+for celestial in array:
+    print(celestial)
+
+
+index = 0 #インデックス初期値
+while index < 9:
+    print(array[index])
+    index = index + 1
