d87dca8c79df1ec720e6ed9cf3960920fbf85446
chicorichan <157950805+chicorichan@users.noreply.github.com>
2024-03-10 14:12:08 +0900

    17章課題を再提出

--- kadai_017.py
+++ kadai_017.py
@@ -1,25 +1,21 @@
 class Human:
-  def __init__(self,name,age):
-    self.name=name
-    self.age=age
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
 
  #メソッドの定義
-  def set_name(self,name):
-    self.name = name
-
-  def set_age(self,age):
-    self.age = age
+    def check_adult(self):
+        if self.age >19:
+            print(f"{self.name}は{self.age}歳なので大人です")
+        else:
+            print(f"{self.name}は{self.age}歳なので大人ではありません")
 
 #インスタンス化
-human = Human("侍太郎",36)
-human1 = Human("侍花子",12)
-human2 = Human("侍一郎",20)
+human = Human("侍太郎", 36)
+human1 = Human("侍花子", 12)
+human2 = Human("侍一郎", 20)      
 
-users = {human.name:human.age, human1.name:human1.age, human2.name:human2.age}
-
-for key, value in users.items():
-    #print(f"{key}は{value}です")
-    if value > 19:
-      print(f"{key}は{value}歳の大人です。")
-    else:
-      print(f"{key}は{value}歳なので大人ではありません。")
+users = {1:human, 2:human1, 3:human2}
+ 
+for human.value in users:
+    human.check_adult()
